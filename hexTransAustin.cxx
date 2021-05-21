1bf81c3c064955e2487948a0b26f7d0bd6017c25
aschmier <aschmier@vols.utk.edu>
2021-05-21 16:37:04 -0400

    hexTrans edits

---
+++ hexTransAustin.cxx
@@ -0,0 +1,105 @@
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void printHeader(string str){
+  stringstream ss(str);
+  string word;
+  int wordNumber = 0;
+  string headerVersion;
+  string headerSize;
+  string feeID;
+  string priorityBit;
+  string sourceID;
+  string beamCounter;
+  string orbit;
+  string triggerType;
+  string pagesCounter;
+  string stopBit;
+  string detectorField;
+  string parBit;
+
+  while(ss >> word)
+  {
+    if(wordNumber == 1) headerVersion = word;
+    if(wordNumber == 2) headerSize = word;
+    if(wordNumber == 3 || wordNumber == 4) feeID = word + feeID;
+    if(wordNumber == 5) priorityBit = word;
+    if(wordNumber == 6) sourceID = word;
+    if(wordNumber == 11 || wordNumber == 12) beamCounter = word + beamCounter;
+    if(wordNumber == 15 || wordNumber == 16 || wordNumber == 17 || wordNumber == 18) orbit = word + orbit;
+    if(wordNumber == 21 || wordNumber == 22 || wordNumber == 23 || wordNumber == 24) triggerType = word + triggerType;
+    if(wordNumber == 25 || wordNumber == 26) pagesCounter = word + pagesCounter;
+    if(wordNumber == 27) stopBit = word;
+    if(wordNumber == 34 || wordNumber == 35 || wordNumber == 36 || wordNumber == 37) detectorField = word + detectorField;
+    if(wordNumber == 38 || wordNumber == 39) parBit = word + parBit;
+
+    wordNumber++;
+  }
+  cout << "Header Version: " << stoi(headerVersion, 0, 16) << endl;
+  cout << "Header Size: " << stoi(headerSize, 0, 16) << endl;
+  cout << "FEE ID: " << stoi(feeID, 0, 16) << endl;
+  cout << "Priority Bit: " << stoi(priorityBit, 0, 16) << endl;
+  cout << "Source ID: " << stoi(sourceID, 0, 16) << endl;
+  cout << "Beam Counter: " << stoi(beamCounter, 0, 16) << endl;
+  cout << "Orbit: " << stoi(orbit, 0, 16) << endl;
+  cout << "Trigger Type: " << stoi(triggerType, 0, 16) << endl;
+  cout << "Pages Counter: " << stoi(pagesCounter, 0, 16) << endl;
+  cout << "Stop Bit: " << stoi(stopBit, 0, 16) << endl;
+  cout << "Detector Field: " << stoi(detectorField, 0, 16) << endl;
+  cout << "Pause and Recover: " << stoi(parBit, 0, 16) << endl;
+
+  //if(stoi(stopBit, 0, 16) == 0) return 0;
+  //if(stoi(stopBit, 0, 16) == 1) return 1;
+  //else cout << "Error: Stop bit has undefined value." << endl;
+}
+
+bool checkStopBit(string str){
+  stringstream ss(str);
+  string word;
+  int wordNumber = 0;
+  string stopBit;
+  while(ss >> word)
+  {
+    if(wordNumber == 27) stopBit = word;
+    wordNumber++;
+  }
+  if(stoi(stopBit, 0, 16) == 0) return 0;
+  if(stoi(stopBit, 0, 16) == 1) return 1;
+  else cout << "Error: Stop bit has undefined value." << endl;
+}
+
+int main () {
+  string line;
+  ifstream myfile ("hexdump_example.txt");
+  if (myfile.is_open())
+  {
+    int lineNumber = 0;
+    string sensor1;
+
+    while ( getline (myfile,line) )
+    {
+      sensor1.append(line);
+      bool isEnd = checkStopBit(sensor1);
+      if(isEnd) 
+      sensor1 += '\n';
+
+      lineNumber++;
+    }
+    cout << "Sensor 1: " << endl;
+    printHeader(sensor1);
+
+    if(!isEnd){
+      cout << "Reading MVTX specific data..." << endl;
+    }
+
+    myfile.close();
+  }
+
+  else cout << "Unable to open file";
+
+  return 0;
+}
